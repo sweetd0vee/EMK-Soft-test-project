f7e69a902bc67e93a72c20ea965e7b969c77c0d5
Arina Goloubitskaya <arinaogo@gmail.com>
2025-07-11 14:08:56 +0300

    initial commit

---
+++ database/connection.py
@@ -0,0 +1,22 @@
+from decouple import config
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+#SQLALCHEMY_DATABASE_URL = config("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:@localhost:5432/shop"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
